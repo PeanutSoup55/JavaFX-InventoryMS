37817b49d220ada4fbe4076c6c689b85b4704c80
PeanutSoup55 <ericblanchette55hockey@gmail.com>
2026-01-27 18:01:18 -0500

    got rif of the premade slop

---
+++ src/main/java/com/example/javafx_inventoryms/Main.java
@@ -0,0 +1,25 @@
+package com.example.javafx_inventoryms;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Inventory Management System");
+
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 800, 600);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+

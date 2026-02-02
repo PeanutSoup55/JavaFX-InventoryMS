1e3bbe5bdd357536333f899833b7c2b30103c759
PeanutSoup55 <ericblanchette55hockey@gmail.com>
2026-02-02 16:03:14 -0500

    added rank and pay for users to distinguish between their positions in hierarchy and pay

--- src/main/java/com/example/javafx_inventoryms/objects/User.java
+++ src/main/java/com/example/javafx_inventoryms/objects/User.java
@@ -4,11 +4,15 @@ public class User {
     private int id;
     private String username;
     private String password;
+    private String rank;
+    private double pay;
 
-    public User(int id, String username, String password) {
+    public User(int id, String username, String password, String rank, double pay) {
         this.id = id;
         this.username = username;
         this.password = password;
+        this.rank = rank;
+        this.pay = pay;
     }
 
     public int getId() {
@@ -33,5 +37,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public double getPay() {
+        return pay;
+    }
+
+    public void setPay(double pay) {
+        this.pay = pay;
     }
 }
